96ea464cc844a9b93f6ddaf0fcf6484072546765
Ivansa71 <ithuta484@gmail.com>
2025-04-12 13:56:24 +0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import './App.css'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
-const CustomButtonGroup = ({next, previous, ...rest}) => {
+interface ButtonGroupProps {
+    next?: () => void;
+    previous?: () => void;
+    rest?: any;
+}
+
+const CustomButtonGroup = ({ next, previous}: ButtonGroupProps) => {
+    const handleNext = () => {
+        if (next) next();
+    };
+
+    const handlePrevious = () => {
+        if (previous) previous();
+    };
+
     return (
         <div className="carousel-button-group">
-            <button className="carousel-button prev" onClick={() => previous()}>
+            <button className="carousel-button prev" onClick={handlePrevious}>
                 ‹
             </button>
-            <button className="carousel-button next" onClick={() => next()}>
+            <button className="carousel-button next" onClick={handleNext}>
                 ›
             </button>
         </div>
@@ -20,7 +34,7 @@ function App() {
 
 
     return (
-        <div className= 'portfolio'>
+        <div className='portfolio'>
             <header>
                 <ul>
                     <li>Главная</li>
@@ -29,22 +43,22 @@ function App() {
                     <li>Контакты</li>
                 </ul>
             </header>
-            <section>
+            <section className='main'>
                 <img src='src/assets/images/photo_2025-03-03_13-32-51.png' alt=""/>
                 <div>
                     <h1>Дизайн интерьеров</h1>
                     <hr/>
-                    <p>Мои проекты - это внешнее отражение вашего внутреннего мира</p>
+                    <p className='mainSlogan'> Мои проекты - это внешнее отражение вашего внутреннего мира</p>
                     <p>Я — мастер спорта России по художественной гимнастике. В 2021 году я завершила обучение в
                         Российском экономическом университете имени Г.В. Плеханова на факультете экономики и права. В
                         настоящее время работаю в сфере коммуникаций между бизнесом и властью, где приобрела ценный опыт
                         взаимодействия с различными заинтересованными сторонами.
                     </p>
-                        <p>Однако со временем я осознала, что моя настоящая страсть заключается в дизайне интерьеров. Этот
+                    <p>Однако со временем я осознала, что моя настоящая страсть заключается в дизайне интерьеров. Этот
                         интерес пробудился во время ремонта в моей квартире, когда я поняла, как важно создавать
                         комфортные и эстетически привлекательные пространства для жизни людей. Желание реализовать свои
                         идеи и решения в области дизайна стало для меня приоритетом.</p>
-                       <p> Для дальнейшего развития своих навыков я решила обучиться в Московской школе дизайна (МШД), что
+                    <p> Для дальнейшего развития своих навыков я решила обучиться в Московской школе дизайна (МШД), что
                         стало отличным стартом моего путешествия в мир дизайна. Я уверена, что полученные знания и опыт
                         позволяют мне создавать уникальные и функциональные интерьеры, которые не только радуют глаз, но
                         и делают жизнь людей более комфортной и гармоничной.
@@ -52,13 +66,13 @@ function App() {
                     {/*<img className='diplom' src='src/assets/images/ДИПЛОМ.jpg' alt=""/>*/}
                 </div>
             </section>
-            <section>
+            <section className='info'>
                 <hr/>
                 <h2>
                     Для меня важен грамотный и системный подход
                 </h2>
                 <hr/>
-                <div>
+                <div className='infoContainer'>
                     <div>
                         <span>01</span>
                         <h3>Комплексное ведение</h3>
@@ -289,7 +303,7 @@ function App() {
                     </div>
                 </Carousel>
             </section>
-            <section>
+            <section className= 'contact'>
                 <div>
                     <h3>
                         ОБСУДИМ ДЕТАЛИ?
@@ -327,25 +341,15 @@ function App() {
                             required
                         />
                     </div>
-                    <div className="input-group">
-                        <label htmlFor="message">Сообщение</label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            required
-                        ></textarea>
-                    </div>
                     <button type="submit" className="submit-button">
                         Отправить
                     </button>
                 </form>
             </section>
-            <footer>
-                <div>
+            <footer className='footer'>
                     <span>Заказать проект</span>
                     <a href="tel:+79999999999">+7 999 999-99-99</a>
                     <a href="mailto:info@example.com">info@example.com</a>
-                </div>
             </footer>
         </div>
     )
